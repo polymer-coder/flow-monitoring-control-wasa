46e9e245ed4c6d0184071fd2b6929ef50cec47da
polymer-coder <developer.polymer@gmail.com>
2023-01-03 16:53:18 -0400

    synchronized controllino state with app state on reset

--- src/main.cpp
+++ src/main.cpp
@@ -16,10 +16,12 @@
 
 // System configuration values
 #define CONTROLLINO_BAUD_RATE 921600
+#define ANALOG_OUTPUT_AIR_PRESSURE CONTROLLINO_AO0 // Hardware modded channel to get current output instead of voltage
+#define ANALOG_INPUT_FLOWRATE CONTROLLINO_AI12     // this input is a special 0-10V input. The remaining ore 0-24V
 
 // Pipe dimensions for flow meter
 #define PIPE_INTERNAL_DIAMETER 0.0508      // 2" = 0.0508m
-#define PIPE_INTERNAL_RADIUS 0.0254        // 2" = 0.0508m
+#define PIPE_INTERNAL_RADIUS 0.0254        // 1" = 0.0254m
 #define PIPE_CROSS_SECTION_AREA 2.02683e-3 // πr2
 #define m_CUBED_PER_SEC_TO_m_CUBED_PER_HOUR 3600
 
@@ -53,9 +55,9 @@ void getControllerUptime()
   const int min = Controllino_GetMinute();
   const int sec = Controllino_GetSecond();
 
-  const long unsigned int startSecs = time[3] * 3600 + time[4] * 60 + time[5]; 
-  const long unsigned int endSecs = hrs * 3600 + min * 60 + sec; 
-  long unsigned int diff = endSecs-startSecs;
+  const long unsigned int startSecs = time[3] * 3600 + time[4] * 60 + time[5];
+  const long unsigned int endSecs = hrs * 3600 + min * 60 + sec;
+  long unsigned int diff = endSecs - startSecs;
 
   int systemUpTimeMn = int((diff / (60)) % 60);
   int systemUpTimeHr = int((diff / (60 * 60)) % 24);
@@ -77,14 +79,19 @@ void myTimerEvent()
   Blynk.virtualWrite(BLYNK_BLEACH_FLOWRATE, flowrate);
 }
 
+BLYNK_CONNECTED()
+{
+  Blynk.syncAll();
+}
+
 BLYNK_WRITE(BLYNK_FLOWRATE_SETPOINT) // this command is listening when something is written to V1
 {
   int pinValue = param.asFloat(); // assigning incoming value from pin V1 to a variable
 
   Serial.print("V1 button value is: "); // printing value to serial monitor
   Serial.println(pinValue);
-  const int adcVal = map(pinValue,0,100,51,252);
-  analogWrite(CONTROLLINO_AO0, adcVal);
+  const int adcVal = map(pinValue, 0, 100, 51, 252);
+  analogWrite(ANALOG_OUTPUT_AIR_PRESSURE, adcVal);
   // map pinValue to current output
 }
 
@@ -119,20 +126,21 @@ void setup()
   Serial.println(onlineSinceDate);
 
   // Physical pin and relay configuration
-  pinMode(CONTROLLINO_D0, OUTPUT);
-  pinMode(CONTROLLINO_AO1, OUTPUT); // 4-20mA out to control valve for air supply
-  pinMode(CONTROLLINO_AI12, INPUT); // 0-10V input from INOR transmitter. Used to convert 4-20mA from flowmeter to 0-10V input to flow meter
+  pinMode(ANALOG_OUTPUT_AIR_PRESSURE, OUTPUT); // 4-20mA out to control valve for air supply
+  pinMode(ANALOG_INPUT_FLOWRATE, INPUT);       // 0-10V input from INOR transmitter. Used to convert 4-20mA from flowmeter to 0-10V input to flow meter
 
-  //
-  analogWrite(CONTROLLINO_AO0, 51); //set analog output to 4ma
+  
+  analogWrite(ANALOG_OUTPUT_AIR_PRESSURE, 51); // set analog output to 4mA
 
-  //TODO: Then after blynk boots up, synchronize the value of the app to the Controllino
-   Blynk.begin(auth);
+  Blynk.begin(auth);
   // Setup a function to be called every second
   timer.setInterval(1000L, myTimerEvent);
 
   Blynk.logEvent("low_flow_-_10_minutes");
   /*Blynk.logEvent("event_code", String("High TemperatureDetected! Tº: ") + temp);*/
+
+  // After blynk boots up, synchronize the value of the Blynk app to the Controllino
+  Blynk.syncVirtual(BLYNK_FLOWRATE_SETPOINT);
 }
 
 void loop()
@@ -140,15 +148,15 @@ void loop()
 
   Blynk.run();
   timer.run(); // Initiates BlynkTimer
-  float sensorValue = analogRead(CONTROLLINO_AI12);
-  Serial.print(sensorValue);
+  float sensorValue = analogRead(ANALOG_INPUT_FLOWRATE);
+  // Serial.print(sensorValue);
   sensorValue = sensorValue * (5.0 / 1023.0);
-  Serial.print(",");
-  Serial.print(sensorValue);
+  // Serial.print(",");
+  // Serial.print(sensorValue);
   flowrate = (sensorValue * PIPE_CROSS_SECTION_AREA) * m_CUBED_PER_SEC_TO_m_CUBED_PER_HOUR;
-  Serial.print(",");
-  Serial.println(flowrate);
-  //getControllerUptime();
+  // Serial.print(",");
+  // Serial.println(flowrate);
+  // getControllerUptime();
 }
 
 /* #include <Arduino.h>
@@ -157,7 +165,7 @@ void loop()
 // the setup function runs once when you press reset (CONTROLLINO RST button) or connect the CONTROLLINO to the PC
 void setup() {
  // initialize all used digital output pins as outputs
- pinMode(CONTROLLINO_AO0, OUTPUT);
+ pinMode(ANALOG_OUTPUT_AIR_PRESSURE, OUTPUT);
  pinMode(CONTROLLINO_AO1, OUTPUT);
 }
 
@@ -165,6 +173,6 @@ void setup() {
 void loop() {
  int analogOut0 = 127;                     // 0 - 255 to be set (0 - 10 000 mV, or 0 - 20 000 uA)
  int analogOut1 = 255;                     // 0 - 255 to be set (0 - 10 000 mV, or 0 - 20 000 uA)
- analogWrite(CONTROLLINO_AO0, analogOut0); // set the analog output 0 to 5V or 10mA
+ analogWrite(ANALOG_OUTPUT_AIR_PRESSURE, analogOut0); // set the analog output 0 to 5V or 10mA
  analogWrite(CONTROLLINO_AO1, analogOut1); // set the analog output 1 to 10V or 20mA
 } */
